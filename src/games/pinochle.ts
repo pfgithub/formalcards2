b58c553a3d000a2b933bed69ff6139db910759c8
pfg <pfg@pfg.pw>
2024-12-26 18:21:34 -0500

    pinochle almost done

--- src/games/pinochle.ts
+++ src/games/pinochle.ts
@@ -4,9 +4,8 @@ type State = {
     deck: Pile,
     players: PlayerCircle,
     hands: Map<Player, Hand>,
+    winnings: Map<Player, Pile>,
     in_play: Pile,
-    t1_winnings: Pile,
-    t2_winnings: Pile,
 
     bid?: number,
     trump?: Suit,
@@ -41,27 +40,35 @@ type Action = {
     card: Card,
 };
 
-function scoreHasEvery(meld: Set<Card>, has: [Value, Suit][], score: number, double_score: number): number {
-    let single_count = 0;
-    let double_count = 0;
+function scoreHasEvery(meld: Map<Card, boolean>, has: [Value, Suit][], score: number, double_score: number): number {
+    let single_count: Card[] = [];
+    let double_count: Card[] = [];
     for(const itm of has) {
-        const c = [...meld].filter(c => c.suit === itm[1] && c.value === itm[0]);
-        if(c.length >= 1) single_count += 1;
-        if(c.length >= 2) double_count += 1;
+        const c = [...meld.keys()].filter(c => c.suit === itm[1] && c.value === itm[0]);
+        if(c.length >= 1) single_count.push(c[0]);
+        if(c.length >= 2) double_count.push(c[0], c[1]);
     }
-    if(double_count === has.length) return double_score;
-    if(single_count === has.length) return score;
+    if(double_count.length === has.length) {
+        double_count.forEach(dc => meld.set(dc, true));
+        return double_score;
+    }
+    if(single_count.length === has.length) {
+        single_count.forEach(dc => meld.set(dc, true));
+        return score;
+    }
     return 0;
 }
 const suits = [Suit.hearts, Suit.spades, Suit.diamonds, Suit.clubs];
-function scoreNAround(meld: Set<Card>, value: Value, score: number, double_score: number): number {
+const values = [Value.nine, Value.jack, Value.queen, Value.king, Value.ten, Value.ace];
+function scoreNAround(meld: Map<Card, boolean>, value: Value, score: number, double_score: number): number {
     return scoreHasEvery(meld, suits.map(suit => [value, suit]), score, double_score);
 }
-function scoreRun(meld: Set<Card>, trump: Suit, score: number, double_score: number): number {
+function scoreRun(meld: Map<Card, boolean>, trump: Suit, score: number, double_score: number): number {
     const run_cards = [Value.jack, Value.queen, Value.king, Value.ten, Value.ace];
     return scoreHasEvery(meld, run_cards.map(rc => [rc, trump]), score, double_score);
 }
-function scoreMeld(trump: Suit, meld: Set<Card>): number {
+function scoreMeld(trump: Suit, meld_in: Set<Card>): number {
+    const meld = new Map([...meld_in].map(m => [m, false]));
     // kq of trump, not in run = 4
     // kq = 2
 
@@ -71,13 +78,31 @@ function scoreMeld(trump: Suit, meld: Set<Card>): number {
     score += scoreNAround(meld, Value.king, 8, 80); // (double)? kings around
     score += scoreNAround(meld, Value.queen, 6, 60); // (double)? queens around
     score += scoreNAround(meld, Value.jack, 4, 40); // (double)? jacks around
-    score += [...meld].filter(c => c.suit === trump && c.value === Value.nine).length; // nines of trump
+    score += [...meld.keys()].filter(c => c.suit === trump && c.value === Value.nine).map(m => meld.set(m, true)).length; // nines of trump
     score += scoreHasEvery(meld, [[Value.jack, Suit.diamonds], [Value.queen, Suit.spades]], 4, 30); // (double)? pinochle
     for(const suit of suits) {
         score += scoreHasEvery(meld, [[Value.king, suit], [Value.queen, suit]], suit === trump ? 4 : 2, suit === trump ? 8 : 4);
     }
 
+    if(![meld.values()].every(v => v)) invalid("unnecessary card melded");
+
     return score;
+}
+
+function getWinningCard(trump: Suit, cards: Card[]): Card {
+    const lead = cards[0];
+    let highest = lead;
+    for(let i = 1; i < cards.length; i++) {
+        const card = cards[i];
+        if(card.suit == trump) {
+
+        }else if(card.suit == lead.suit) {
+
+        }else{
+            continue;
+        }
+    }
+    return highest;
 }
 
 function* game(players_in: PlayerCircle): Generator<undefined, void, Action> {
@@ -88,12 +113,14 @@ function* game(players_in: PlayerCircle): Generator<undefined, void, Action> {
         deck: pinochleDeck(),
         players: players_in,
         hands: new Map(players_in.players.map(p => [p, new Hand(p)])),
+        winnings: new Map(),
         in_play: new Pile(),
 
-        t1_winnings: new Pile(),
-        t2_winnings: new Pile(),
-
         passed: new Set(),
+
+        // seen_nines: new Set(),
+        // if during the game you see a nine which was not counted towards meld, you can retroactively add it.
+        // 'claim_forgotten_nine'
     };
 
     const deck = pinochleDeck();
@@ -178,6 +205,8 @@ function* game(players_in: PlayerCircle): Generator<undefined, void, Action> {
         if(action.kind !== "play_card") invalid("can't do that now");
         if(action.player !== leader) invalid("not your turn");
         state.in_play.add(state.hands.get(action.player)!.take(action.card), Facing.up);
+        const lead_card = action.card;
+        const players: Player[] = [action.player];
         // 3 more ppl play cards
         let player = leader;
         do {
@@ -186,25 +215,46 @@ function* game(players_in: PlayerCircle): Generator<undefined, void, Action> {
             const action = yield;
             if(action.kind !== "play_card") invalid("can't do that now");
             if(action.player !== player) invalid("not your turn");
-            // TODO:
-            // -> if has suit, must follow suit
-            //     - if can beat, must (= have to play a higher card unless trump was played)
-            // -> if has trump, must trump
-            //     - if can beat, must (= have to play a higher trump if a trump has been played)
-            // -> may play any card
-            if((true)) invalid("TODO validate card");
+
+            let options = state.hands.get(action.player!)!.cards;
+            const follow_suit_options = options.filter(card => card.suit === lead_card.suit);
+            const trump_options = options.filter(card => card.suit === lead_card.suit);
+            if(follow_suit_options.length > 0) {
+                // follow suit if can
+                options = follow_suit_options;
+            }else if(trump_options.length > 0) {
+                // else trump if can
+                options = trump_options;
+            }
+            // beat if can
+            const can_win_options = options.filter(opt => getWinningCard(state.trump!, [...state.in_play.cards, opt]) === opt);
+            if(can_win_options.length > 0) options = can_win_options;
+
+            if(!options.includes(action.card)) invalid("must follow suit if can, else trump if can. must beat if can.");
             state.in_play.add(state.hands.get(action.player)!.take(action.card), Facing.up);
+            players.push(action.player);
         }while(player != leader);
 
         // cards are given to the stack & next leader is chosen
+        const winning_card = getWinningCard(state.trump!, state.in_play.cards);
+        const winning_player = players[state.in_play.cards.indexOf(winning_card)];
+        const teammate = state.players.oppositeOf(winning_player);
+        if(state.winnings.has(winning_player)) {
+            state.winnings.get(winning_player)!.addAll(state.in_play.takeAll(), Facing.down); // but you are still allowed to review the last trick if you forget
+        }else{
+            // generally for player that won the bid, always the teammate takes the pile even in the rare case where they
+            // take the first trick. so this isn't quite right.
+            if(!state.winnings.has(teammate)) state.winnings.set(teammate, new Pile());
+            state.winnings.get(teammate)!.addAll(state.in_play.takeAll(), Facing.down);
+        }
 
-        if((true)) invalid("TODO choose next leader");
+        leader = winning_player;
     }
-
-    // TODO: allow late counting of a forgotten 9 during melding (nothing else though)
 
     // count score
     // ace, ten, king = +1, else = +0
     // 0 points = don't keep your meld (even if you took a trick)
     if((true)) invalid("TODO count score");
+
+    // and then loop the whole game four times with the different dealers
 }
